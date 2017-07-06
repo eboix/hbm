9a3b34bf52521ec86ba5f43ddd6df2271fdf528e
eboix <e.boix.adsera@gmail.com>
2017-07-06 12:09:11 -0400

    Added heatmap code.

--- hbm_stats_parser.m
+++ hbm_stats_parser.m
@@ -14,6 +14,7 @@ dval = zeros(num_files,1);
 T = zeros(num_files,1);
 Dval = cell(num_files,1);
 row_names = cell(num_files,1);
+GiantNs = cell(num_files,1);
 for i = 1:num_files;
 if mod(i,100) == 0
     i
@@ -29,6 +30,7 @@ C(i) = c;
 dval(i) = d;
 T(i) = t;
 Dval{i} = D;
+GiantNs{i} = giant_ns;
 end
 methodname = categorical(MethodName);
 res = Res;
@@ -39,4 +41,29 @@ c = C;
 d = dval;
 t = T;
 D = Dval;
-vals = table(methodname,res,n,a,b,c,d,t,dval);
+giant_n = GiantNs;
+T = table(methodname,res,n,a,b,c,d,t,D,giant_n);
+
+Trn = T((T.methodname == 'randwalk') & (T.n == 6000) & (T.t == 1),:);
+drange = 0:0.1:2.5;
+crange = 4:2:20;
+di = 0;
+for d = 0:0.1:2.5
+    di = di + 1;
+    ci = 0;
+    for c = crange
+      %  c
+       % d
+        ci = ci + 1;
+        imgres(di,ci) = Trn(Trn.c == c & Trn.d == d,:).res;
+    end
+end
+
+mvals = cellfun(@(x) sum(x)/length(x), imgres);
+% mvals = real(cellfun(@(x) x, imgres));
+color_res = 1024;
+colormap(hot(color_res));
+heatmap(mvals,crange,drange,'%0.2f','ColorBar',true,'MinColorValue',0.5,'MaxColorValue',1)
+xlabel('c');
+ylabel('d');
+title('randwalk agreement, avg of 5 trials');

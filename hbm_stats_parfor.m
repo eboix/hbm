616561e843671f786185f6635613c84771678d19
eboix <e.boix.adsera@gmail.com>
2017-07-06 03:13:04 -0400

    Test nbwalk

--- hbm_stats_parfor.m
+++ hbm_stats_parfor.m
@@ -1,11 +1,12 @@
 maxNumCompThreads(20)
-n_vals = 2000:2000:10000;
+n_vals = 2000:4000:20000;
 d_vals = 0:0.2:2.4;
 [N,D] = meshgrid(n_vals,d_vals);
 parfor iter=1:length(N(:));
     n = N(iter);
     d = D(iter);
+   
     for c = 2:10
-        hbm_stats('adj',n,0,0,c*2,d,1,10,'/home/eboix/sum17/res/')
+        hbm_stats('nbwalk',n,0,0,c*2,d,1,5,'~/sum17/res/')
     end
 end

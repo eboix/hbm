a8bcd14ca1b2d27ebc248ee80ecbb3f3ffd56c84
eboix <e.boix.adsera@gmail.com>
2017-07-06 02:50:43 -0400

    Figured out error

--- hbm_stats_parfor.m
+++ hbm_stats_parfor.m
@@ -2,7 +2,7 @@ maxNumCompThreads(20)
 n_vals = 2000:2000:10000;
 d_vals = 0:0.2:2.4;
 [N,D] = meshgrid(n_vals,d_vals);
-parfor iter=1:length(N);
+parfor iter=1:length(N(:));
     n = N(iter);
     d = D(iter);
     for c = 2:10

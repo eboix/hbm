3a788bd213bddc0c8abc1b2b49cf7aa6b31e5222
Enric Boix <e.boix.adsera@gmail.com>
2017-07-06 01:49:49 -0400

    Multithreaded.

--- hbm_batch.bat
+++ hbm_batch.bat
@@ -6,4 +6,4 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=eboix@princeton.edu
 cd ~/sum17
-/usr/licensed/bin/matlab -singleCompThread -nodisplay -nosplash -nojvm -r hbm_stats_parfor
+/usr/licensed/bin/matlab -nodisplay -nosplash -nojvm -r hbm_stats_parfor

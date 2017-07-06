5a3c7dc1867b5c37b7efb566f0aa0d23eb55ee83
Enric Boix <e.boix.adsera@gmail.com>
2017-07-06 13:01:07 -0400

    Parallelized a bit more.

--- hbm_stats_parfor.m
+++ hbm_stats_parfor.m
@@ -1,12 +1,11 @@
 maxNumCompThreads(20)
-n_vals = 2000:2000:20000;
+n_vals = 20000;
 d_vals = 0:0.2:2.4;
-[N,D] = meshgrid(n_vals,d_vals);
+c_vals = 6:4:20;
+[N,D,C] = meshgrid(n_vals,d_vals,c_vals);
 parfor iter=1:length(N(:));
     n = N(iter);
     d = D(iter);
-   
-    for c = 2:10
-        hbm_stats('high_deg',n,0,0,c*2,d,1,5,'~/sum17/res/')
-    end
+    c = C(iter);
+    hbm_stats('nbwalk',n,0,0,c,d,1,5,'~/sum17/res/')
 end

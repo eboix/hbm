30f31bfd675f845730f90ed7a00c17e8753ff058
Enric Boix <e.boix.adsera@gmail.com>
2017-07-07 10:21:54 -0400

    Modified ZIP

--- hbm_stats_exec_job.m
+++ hbm_stats_exec_job.m
@@ -1,7 +1,7 @@
 function hbm_stats_exec_job(job_num)
 n_vals = 20000;
-d_vals = 0:0.2:2.4;
-c_vals = 6:2:20;
+d_vals = 0:0.1:2.4;
+c_vals = 6:1:20;
 [N,D,C] = meshgrid(n_vals,d_vals,c_vals);
 
 if nargin == 0 || job_num == -1
@@ -15,5 +15,5 @@ end
 n = N(job_num);
 d = D(job_num);
 c = C(job_num);
-hbm_stats('nbwalk',n,0,0,c,d,1,5,'res/')
+hbm_stats('adj',n,0,0,c,d,1,5,'res/')
 end

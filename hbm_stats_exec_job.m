ac952a6b5ed55ca9653408c4dfe7841b27dd2b11
Enric Boix <e.boix.adsera@gmail.com>
2017-07-09 00:15:27 -0400

    Some n10000 results.

--- hbm_stats_exec_job.m
+++ hbm_stats_exec_job.m
@@ -1,7 +1,7 @@
 function hbm_stats_exec_job(job_num)
 
 methodname = 'nbwalk';
-n_vals = [100 200 400 1000:1000:2000];
+n_vals = 10000;
 d_vals = 0:0.05:4;
 c_vals = 5:0.05:20;
 [N,D,C] = meshgrid(n_vals,d_vals,c_vals);
@@ -45,6 +45,10 @@ for iter=begin_raw_job:end_raw_job
     n = N(perm_iter);
     d = D(perm_iter);
     c = C(perm_iter);
-    
+
+    if (d-2)*10 > c
+	continue
+    end    
+
     hbm_stats(methodname,n,0,0,c,d,1,1,sprintf('res/%s/n%d/',methodname,n),false);
 end

07aa9f263c392626ce6860aa2973cd660b3a4f4f
Enric Boix <e.boix.adsera@gmail.com>
2017-07-24 14:34:56 -0400

    Added data
    USE_KMEANS = true; % Only supported by nb_classifier.
    methodname = 'graph_pow_lap';
    n_vals = [20000];
    a_vals = 2:0.05:2.5;
    b_vals = 0:0.05:0.5;
    c_vals = [1 2 3 4 5];
    d_vals = 0;
    t_vals = [0 0.05 0.1 0.5 0.9 0.95 1];
    optional_param_vals = [1 2 3 4];

--- hbm_stats_exec_job.m
+++ hbm_stats_exec_job.m
@@ -7,10 +7,10 @@ methodname = 'graph_pow_lap';
 n_vals = [20000];
 a_vals = 2:0.05:2.5;
 b_vals = 0:0.05:0.5;
-c_vals = [5];
+c_vals = [1 2 3 4 5];
 d_vals = 0;
 t_vals = [0 0.05 0.1 0.5 0.9 0.95 1];
-optional_param_vals = [1];
+optional_param_vals = [1 2 3 4];
 
 [NV,AV,BV,CV,DV,TV,OV] = ndgrid(n_vals,a_vals,b_vals,c_vals,d_vals,t_vals,optional_param_vals);
 

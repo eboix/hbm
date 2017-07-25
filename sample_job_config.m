f06b3d9a7517be8985a7b285b617aa77d3da41f6
eboix <e.boix.adsera@gmail.com>
2017-07-25 16:42:14 -0400

    sample_job_config changes.

--- sample_job_config.m
+++ sample_job_config.m
@@ -1,3 +1,4 @@
+%%%%%%%%% HBM_STATS_EXEC_JOB options.
 % Only supported by nb_classifier, graph_pow_lap, graph_pow_adj, graph_pow_adj_trunc.
 global USE_KMEANS;
 USE_KMEANS = true;
@@ -9,4 +10,7 @@ b_vals = 0:0.05:1;
 c_vals = 1;
 d_vals = 0;
 t_vals = [0 0.05 0.1 0.5];
-optional_param_vals = 1:15;
+optional_param_vals = 1:15;optional_param_vals = 1:15;
+
+%%%%%%%%%% HBM_STATS_PARSER options.
+ABPLOT=true;

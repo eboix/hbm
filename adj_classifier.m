f08f923e263847ed915bd43071b884abef3c2bce
eboix <e.boix.adsera@gmail.com>
2017-07-25 22:10:46 -0400

    Added kmeans capability to some adj and randwalk.

--- adj_classifier.m
+++ adj_classifier.m
@@ -1,23 +1,31 @@
 function [class,V,D] = adj_classifier(obj,giant_A,giant_rev)
-    % obj is a hybrid_block_model object.
-    % Optional giant_A and giant_rev arguments.
-    % Use second eigval of A to split vertices.
-    % TODO DOES NOT DEPEND ON obj.k.
+% obj is a hybrid_block_model object.
+% Optional giant_A and giant_rev arguments.
+% Use second eigval of A to split vertices.
+% TODO DOES NOT DEPEND ON obj.k.
 
-    assert(nargin == 1 || nargin == 3);
-    
- %   disp('Running adj_classifier');
-    if nargin == 1
-        [giant_A,~,giant_rev,~,~] = get_giant_adj_matrix(obj);
-    end
-    n = obj.n;
-    giant_n = length(giant_A);
-    [V,D] = eigs(giant_A,2);
+assert(nargin == 1 || nargin == 3);
 
-    classeigvec = V(:,2);
+%   disp('Running adj_classifier');
+if nargin == 1
+    [giant_A,~,giant_rev,~,~] = get_giant_adj_matrix(obj);
+end
+n = obj.n;
+giant_n = length(giant_A);
+[V,D] = eigs(giant_A,2);
+
+classeigvec = V(:,2);
+global USE_KMEANS
+if USE_KMEANS
+    C = kmeans(classeigvec,obj.k,'replicates',10);
+    class = zeros(n,1);
+    class(giant_rev(C == 1)) = 1;
+    class(giant_rev(C == 2)) = 2;
+else
     [~,idx] = sort(classeigvec);
     class = zeros(n,1);
     class(giant_rev(idx(1:floor(giant_n/2)))) = 1;
     class(giant_rev(idx(floor(giant_n/2):end))) = 2;
-        
+end
+
 end
--- queued_jobs/adj_gbm.m
+++ queued_jobs/adj_gbm.m
@@ -3,14 +3,14 @@
 global USE_KMEANS;
 USE_KMEANS = true;
 
-methodname = 'graph_pow_adj_trunc_weighted';
+methodname = 'adj';
 n_vals = [20000];
 a_vals = 0;
 b_vals = 0;
 c_vals = 2:0.1:15;
 d_vals = 0:0.1:3;
 t_vals = 1;
-optional_param_vals = [1:5 10 15];
+optional_param_vals = -1;
 
 %%%%%%%%%% HBM_STATS_PARSER options.
 ABPLOT=false;

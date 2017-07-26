ce51e4480e02753b5b3bf9754de09c3d37029574
eboix <e.boix.adsera@gmail.com>
2017-07-26 10:22:41 -0400

    Added some graph_pow_adj_trunc_sbm jobs.

--- queued_jobs/graph_pow_adj_trunc_sbm.m
+++ queued_jobs/graph_pow_adj_trunc_sbm.m
@@ -5,12 +5,12 @@ USE_KMEANS = true;
 
 methodname = 'graph_pow_adj_trunc';
 n_vals = [20000];
-a_vals = 2:0.1:3;
-b_vals = 0:0.1:1;
+a_vals = 2:0.05:3;
+b_vals = 0:0.05:1;
 c_vals = 0;
 d_vals = 0;
 t_vals = 0;
 optional_param_vals = [1:5 10 15];
 
 %%%%%%%%%% HBM_STATS_PARSER options.
-ABPLOT=false;
+ABPLOT=true;
